dfeab48ecd8155d08a4589c9886d937cde6a3381
Marios2 <kosma@ceid.upatras.gr>
2019-03-31 22:10:49 +0300

    demo

---
+++ demo/precache-manifest.44663704b93eb0b389498a04fc762850.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "abbd1dbfccbda157978f697d1e993ce4",
+    "url": "./static/media/cereal-563796_1920.abbd1dbf.jpg"
+  },
+  {
+    "revision": "67990aefa103fcd6011d",
+    "url": "./static/css/main.a26ae12b.chunk.css"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8d2bb0f50850b5bc594259d2a63e3916",
+    "url": "./static/media/kitchen-649110_1920.8d2bb0f5.jpg"
+  },
+  {
+    "revision": "9b3cb9cf0441ee60b506",
+    "url": "./static/js/2.c86ba5f5.chunk.js"
+  },
+  {
+    "revision": "a3c0030a936077b2b9ee1332992f0086",
+    "url": "./static/media/salad-2068220_1920.a3c0030a.jpg"
+  },
+  {
+    "revision": "e612acd7cdf301ba3b310d4deb8cde89",
+    "url": "./static/media/blueberries-2278921_1920.e612acd7.jpg"
+  },
+  {
+    "revision": "61fec86978122c9cb8b2f5ad20c012de",
+    "url": "./static/media/casserole-dish-2776735_1920.61fec869.jpg"
+  },
+  {
+    "revision": "67990aefa103fcd6011d",
+    "url": "./static/js/main.4385b22f.chunk.js"
+  },
+  {
+    "revision": "0b62c53d4d5502b21c5eeee381e0b4f6",
+    "url": "./static/media/shish-kebab-417994_1920.0b62c53d.jpg"
+  },
+  {
+    "revision": "68722edd24c210133c4cddb4b0eb2d1f",
+    "url": "./static/media/vegetables-1085063_1920.68722edd.jpg"
+  },
+  {
+    "revision": "7a36e903ab89b41b0a1d344322bf44cf",
+    "url": "./static/media/fruit-2305192__340.7a36e903.jpg"
+  },
+  {
+    "revision": "0d910888e7363a176ae0e730d581fffd",
+    "url": "./static/media/asparagus-2169305_1920.0d910888.jpg"
+  },
+  {
+    "revision": "cd35b3cd4d7f0c2210035925cb87c80f",
+    "url": "./static/media/chef-939436_1920.cd35b3cd.jpg"
+  },
+  {
+    "revision": "9b3cb9cf0441ee60b506",
+    "url": "./static/css/2.b040295f.chunk.css"
+  },
+  {
+    "revision": "517499b6f1bb26009ffb86582f66cf23",
+    "url": "./index.html"
+  }
+];
