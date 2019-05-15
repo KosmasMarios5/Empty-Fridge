6a596edb181c3fb5a6c7239d3374fd4c68024cba
AlexKrets <49129666+AlexKrets@users.noreply.github.com>
2019-05-15 17:53:39 +0300

    Update Web.java

--- empty_fridge/src/empty_fridge/Entities/Web.java
+++ empty_fridge/src/empty_fridge/Entities/Web.java
@@ -11,14 +11,32 @@ import empty_fridge.Entities.Recipe;
  * @author Alexandros
  */
 public class Web {
-    Recipe[] recipes;
-    
+// extends Recipe <--------- ?
+   String[] recipes = {"Makaronada" , "Pizza", "KotopouloStoFourno"};
 /* I need to return a list of recipes here  
  * Can the method type returned be an array of "Recipe"?
  * Use void?  
  */
-    public Recipe[] search(){         
-        Recipe[] webRecipes;
+    public String[] search(String ingredients){
+        int done = 0;
+        String[] webRecipes = {" ", " ", " "};
+        while(done == 0){
+            if (makaronada.getIngredient() == ingredients){
+                webRecipes[1] = {"Makaronada"};
+                done = 1;
+            }else if (pizza.getIngredient() == ingredients){
+                webRecipes[2] = {"Pizza"};
+                done = 1;
+            }else if (kotopoulostofourno.getIngredient() == ingredients){
+                webRecipes[3] = {"KotopouloStoFourno"};
+                done = 1;
+            }else{
+                /* error recipe not found */
+                done = 1;
+            }  
+        }
         return webRecipes;
+        /* Omoia me tin search ths Recipe. Epistrefei "recipes"
+        */
     }
 }
