ccedb55aaee51080d233878cd8f95b5af28edcd9
AlexKrets <49129666+AlexKrets@users.noreply.github.com>
2019-05-15 16:05:29 +0300

    Create Web.java

---
+++ empty_fridge/src/empty_fridge/Entities/Web.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empty_fridge.Entities;
+
+import empty_fridge.Entities.Recipe;
+/**
+ *
+ * @author Alexandros
+ */
+public class Web {
+    Recipe[] recipes;
+    
+/* I need to return a list of recipes here  
+ * Can the method type returned be an array of "Recipe"?
+ * Use void?  
+ */
+    public Recipe[] search(){         
+        Recipe[] webRecipes;
+        return webRecipes;
+    }
+}
