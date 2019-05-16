b207f5fe66794bee041860e2c15cc2d16447e292
PetrosTanakas <49129489+PetrosTanakas@users.noreply.github.com>
2019-05-16 18:15:42 +0300

    Add files via upload

--- empty_fridge/src/empty_fridge/Entities/ShoppingList.java
+++ empty_fridge/src/empty_fridge/Entities/ShoppingList.java
@@ -3,7 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package empty_fridge.Entities;
+
+import empty_fridge.Entities.Ingredient;
+import empty_fridge.Entities.Recipe;
 
 /**
  *
