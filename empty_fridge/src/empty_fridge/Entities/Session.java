3f8f3cd5282198d29e977ed299b34507ea110ac6
AlexKrets <49129666+AlexKrets@users.noreply.github.com>
2019-05-16 17:04:42 +0300

    Session v0.01
    Session start

---
+++ empty_fridge/src/empty_fridge/Entities/Session.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empty_fridge.Entities;
+import empty_fridge.Entities.User;
+import empty_fridge.Entities.Ingredient;
+//import empty_fridge.Entities.Filter;
+/**
+ *
+ * @author Alexandros
+ */
+public class Session {
+    int id;
+    int userid;
+    
+    public Session(){
+    }
+    public void save(int filters){
+        /* Save Filters (Allagh Sequence Filters)
+        *  me eisodo filters
+        */
+    }
+    public void save(String ingredients){
+        /* Save Ingredients Allagh Sequence Filters)
+        *  me eisodo ingredients
+        */
+    }
+    public void getSavedSession(int id){
+        /* Me to user id epistrefei ta apothikeumena ingredients & filters
+        *  pou eixe epilexei kai swsei o xrhsths prohgoumenws
+        */
+    }
+}
--- empty_fridge/src/empty_fridge/Entities/Web.java
+++ empty_fridge/src/empty_fridge/Entities/Web.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,76 +11,26 @@ import empty_fridge.Entities.Recipe;
  * @author Alexandros
  */
 public class Web {
-// extends Recipe <--------- ?
-   String[] recipes = {"Makaronada" , "Pizza", "KotopouloStoFourno"};
-/* I need to return a list of recipes here  
- * Can the method type returned be an array of "Recipe"?
- * Use void?  
- */
-    public String[] search(String ingredients){
-        int done = 0;
-        String[] webRecipes = {" ", " ", " "};
-        while(done == 0){
-            if (makaronada.getIngredient() == ingredients){
-                webRecipes[1] = {"Makaronada"};
-                done = 1;
-            }else if (pizza.getIngredient() == ingredients){
-                webRecipes[2] = {"Pizza"};
-                done = 1;
-            }else if (kotopoulostofourno.getIngredient() == ingredients){
-                webRecipes[3] = {"KotopouloStoFourno"};
-                done = 1;
-            }else{
-                /* error recipe not found */
-                done = 1;
-            }  
-        }
-        return webRecipes;
-        /* Omoia me tin search ths Recipe. Epistrefei "recipes"
-        */
-    }
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package empty_fridge.Entities;
+    
+   Recipe[] recipes;
 
-import empty_fridge.Entities.Recipe;
-/**
- *
- * @author Alexandros
- */
-public class Web {
-// extends Recipe <--------- ?
-   String[] recipes = {"Makaronada" , "Pizza", "KotopouloStoFourno"};
-/* I need to return a list of recipes here  
- * Can the method type returned be an array of "Recipe"?
- * Use void?  
- */
-    public String[] search(String ingredients){
+    public void search(String ingredients){
+        /* I need to return a list of recipes here  
+         * Can the method type returned be an array of "Recipe"?
+         * Use void?  
+        */
         int done = 0;
-        String[] webRecipes = {" ", " ", " "};
+        //new String[3] webRecipes = {" ", " ", " "};
         while(done == 0){
-            if (makaronada.getIngredient() == ingredients){
-                webRecipes[1] = {"Makaronada"};
-                done = 1;
-            }else if (pizza.getIngredient() == ingredients){
-                webRecipes[2] = {"Pizza"};
-                done = 1;
-            }else if (kotopoulostofourno.getIngredient() == ingredients){
-                webRecipes[3] = {"KotopouloStoFourno"};
-                done = 1;
+            /*
+            if ( recipe ingredient == this.ingredients){
             }else{
                 /* error recipe not found */
                 done = 1;
-            }  
+            }
+        //return webRecipes;
         }
-        return webRecipes;
+        
         /* Omoia me tin search ths Recipe. Epistrefei "recipes"
         */
-    }
 }
->>>>>>> e867b98f3adc25eaaf3c280aafa81e36f38ad428
