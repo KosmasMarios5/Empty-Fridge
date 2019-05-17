ca6811fac58233193a1bd52661d45bf5f20d9a75
kostislamp <49129483+kostislamp@users.noreply.github.com>
2019-05-17 20:49:34 +0300

    Added Partner.java

---
+++ empty_fridge/src/empty_fridge/Entities/Partner.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empty_fridge.Entities;
+
+import empty_fridge.Entities.CookingTool;
+
+/**
+ *
+ * @author Kostis
+ */
+public class Partner { 
+    String partnerName; // Fantazomai oti einai to brand sthn CookingTool 
+    int partnerId;
+    private CookingTool[] cookingTools;
+    
+    
+    
+    
+}
